f2bb586b64818bb5b9aabf1a8486a621d1cf4e54
DevBrarum <killi0589@gmail.com>
2024-03-07 22:01:25 -0500

    fix and update project

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,14 @@ import React from "react";
 import trashIcon from '../images/Icons/trash.png';
 import heartIcon from '../images/Icons/wheart.png';
 
-function Card({ card, onCardClick }) {
+function Card(props) {
     function handleClick() {
-        onCardClick(card);
+        props.onCardClick(props.card);
     }
 
 
     return (
-        <div key={card._id} className="photos__template">
+        <div key={props.card._id} className="photos__template">
             <div className="photos__content">
                 <button className="photos__trash-button">
                     <img
@@ -19,10 +19,10 @@ function Card({ card, onCardClick }) {
                     />
                 </button>
                 <button className="photos__imgPopup-button" onClick={handleClick}>
-                    <img src={card.link} alt={card.name} className="photos__img" />
+                    <img src={props.card.link} alt={props.card.name} className="photos__img" />
                 </button>
                 <div className="photos__info">
-                    <h2 className="photos__title">{card.name}</h2>
+                    <h2 className="photos__title">{props.card.name}</h2>
                     <div className="photos__like-countainer">
                         <button className="photos__like-button">
                             <img
@@ -31,7 +31,7 @@ function Card({ card, onCardClick }) {
                                 className="icon_like-heart"
                             />
                         </button>
-                        <div className="photos__like-count">{card.likes.length}</div>
+                        <div className="photos__like-count">{props.card.likes.length}</div>
                     </div>
                 </div>
             </div>
